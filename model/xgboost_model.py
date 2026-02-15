e6e8d3fa555b7efa3c7d5ca44159672e7f8accf6
2025aa05459 <2025aa05459@wilp.bits-pilani.ac.in>
2026-02-15 11:16:33 +0530

    Create xgboost_model.py

---
+++ model/xgboost_model.py
@@ -0,0 +1,12 @@
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+
+def train_xgboost(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+    model.fit(X_train, y_train)
+
+    return model, X_test, y_test
